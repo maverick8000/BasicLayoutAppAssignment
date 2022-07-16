fe9a27bdeeb47613a3e471d98ffae57e4157a1b1
Raul Barranco <maverick@Rauls-MacBook-Pro.local>
2022-07-15 22:09:23 -0400

    update 3

--- BasicLayoutApp4/XIBTableViewCell.swift
+++ BasicLayoutApp4/XIBTableViewCell.swift
@@ -33,10 +33,30 @@ class XIBTableViewCell: UITableViewCell {
         
         vc2.text = "Navigation Controller Presentation 2"
         
+        print("I was tapped!")
         //self.superview?.naviga        
                 
     }
     
     
     
+}
+
+extension UIButton {
+
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isHighlighted = true
+        super.touchesBegan(touches, with: event)
+    }
+
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isHighlighted = false
+        super.touchesEnded(touches, with: event)
+    }
+
+    override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isHighlighted = false
+        super.touchesCancelled(touches, with: event)
+    }
+
 }
